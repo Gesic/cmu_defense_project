93b05553cfdb0e10ad4dc529b82082c21d67e057
gesic <gesic84@gmail.com>
2022-07-05 13:51:10 +0900

    [Server] Remove unused file
    Remove unused file
    Change the deprecated class

--- T5Defense_Server/src/main/java/com/defense/server/spring/SpringConfig.java
+++ T5Defense_Server/src/main/java/com/defense/server/spring/SpringConfig.java
@@ -1,17 +1,22 @@
-package com.defense.server.security;
+package com.defense.server.spring;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
+public class SpringConfig {
+
+	@Bean
+	protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception
+	{
 		http.authorizeRequests().antMatchers("/h2-console/**").permitAll();
 		http.csrf().ignoringAntMatchers("/h2-console/**").disable();
 		http.headers().frameOptions().disable();
+
+		return http.build();
 	}
 }
