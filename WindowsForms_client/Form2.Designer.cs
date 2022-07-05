791ffca99fe1c33a06b63c255ed87c1dd6d9748e
DW LIM <lim184@kakao.com>
2022-07-05 17:51:45 +0900

    [Update] Client Production Code

--- WindowsForms_client/Form2.Designer.cs
+++ WindowsForms_client/Form2.Designer.cs
@@ -31,11 +31,12 @@
             this.BTN_SENDOTP = new System.Windows.Forms.Button();
             this.TXT_OTPNUM = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // BTN_SENDOTP
             // 
-            this.BTN_SENDOTP.Location = new System.Drawing.Point(246, 47);
+            this.BTN_SENDOTP.Location = new System.Drawing.Point(246, 63);
             this.BTN_SENDOTP.Name = "BTN_SENDOTP";
             this.BTN_SENDOTP.Size = new System.Drawing.Size(75, 31);
             this.BTN_SENDOTP.TabIndex = 0;
@@ -46,28 +47,40 @@
             // TXT_OTPNUM
             // 
             this.TXT_OTPNUM.Font = new System.Drawing.Font("굴림", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.TXT_OTPNUM.Location = new System.Drawing.Point(12, 49);
+            this.TXT_OTPNUM.Location = new System.Drawing.Point(12, 63);
             this.TXT_OTPNUM.MaxLength = 6;
             this.TXT_OTPNUM.Name = "TXT_OTPNUM";
             this.TXT_OTPNUM.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
             this.TXT_OTPNUM.Size = new System.Drawing.Size(217, 29);
             this.TXT_OTPNUM.TabIndex = 1;
+            this.TXT_OTPNUM.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.TXT_OTPNUM.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TXT_OTPNUM_KeyPress);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("굴림", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Location = new System.Drawing.Point(12, 35);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(309, 15);
             this.label1.TabIndex = 2;
             this.label1.Text = "Please input your OTP number  (6 digit)";
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("굴림", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.Location = new System.Drawing.Point(12, 10);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(260, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "I sent OTP number to your e-mail";
+            // 
             // Form2
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.ClientSize = new System.Drawing.Size(350, 110);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.TXT_OTPNUM);
             this.Controls.Add(this.BTN_SENDOTP);
@@ -92,5 +105,6 @@
         private System.Windows.Forms.Button BTN_SENDOTP;
         private System.Windows.Forms.TextBox TXT_OTPNUM;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
--- lgdemo_w/lgdemo_w.cpp
+++ lgdemo_w/lgdemo_w.cpp
@@ -41,7 +41,6 @@ BOOL ClgdemowApp::InitInstance()
 	if (GetLastError() == ERROR_ALREADY_EXISTS)
 	{
 		return FALSE;
-		//AfxMessageBox(_T("프로그램이 이미 실행 중입니다."));
 	}
 	// 애플리케이션 매니페스트가 ComCtl32.dll 버전 6 이상을 사용하여 비주얼 스타일을
 	// 사용하도록 지정하는 경우, Windows XP 상에서 반드시 InitCommonControlsEx()가 필요합니다.
